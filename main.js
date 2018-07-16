a04f20ec364cf39e04f56ac21d82d25f5db9c311
chuks_nlog <amaechichuks2000@yahoo.com>
2018-07-16 01:49:31 +0100

    adding a Post method

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ let append = function (parent, el) {
 
 //Implementation to fetch data from specified API(using Promise)
 const  url = 'https://intern-challenge.herokuapp.com/persons';
+let  newUrlData = '';
+let postBody ={};
 fetch(url)
     .then(function (resp) {
         return resp.json(); //returns data in JSON  format
@@ -23,7 +25,7 @@ fetch(url)
                 img = createNode('img'),
                 button = createNode('button'),
                 
-                id_name = person.name.split(' ')[0]; //creating unique id for button
+                id_name = person.id; //creating unique id for button
             img.src = person.photo;
             img.alt = person.name;
             
@@ -36,25 +38,76 @@ fetch(url)
             // Get the modal
             const modal = document.getElementById('myModal');
 
-            // Get the image and insert it inside the modal - use its "alt" text as a caption
+            // Get the image and insert it inside the modal - use its 'alt' text as a caption
             let img_modal = document.getElementById(id_name);
-            let modalImg = document.getElementById("img01");
-            let captionText = document.getElementById("caption");
+            let modalImg = document.getElementById('img01');
+            let captionText = document.getElementById('caption');
             img_modal.onclick = function () {
-                modal.style.display = "block";
+                postBody = person;
+                newUrlData = id_name;
+                console.log(newUrlData);
+                modal.style.display = 'block';
                 modalImg.src = img.src;
-                captionText.innerHTML = `Name : ${img.alt} <br> Age: ${person.age} <br> Description: ${person.description} <a href='#'><i class="fa fa-pencil-square-o" style="font-size:24px;color:red"</a></i> `;
+                captionText.innerHTML = `Name : ${img.alt} <br> Age: ${person.age} <br> Description: <span id='mod'>${person.description}</span> <a id='modify' href='#'><i  class='fa fa-pencil-square-o' style='font-size:24px;color:red'</a></i> `;
+
+                
+                // Fetching the edit icon and toggling the edit form
+                const  update = document.getElementsByClassName('fa')[0];
+                //console.log(update);
+                const modle = document.getElementById('mod');
+                const edit = document.getElementById('to_edit');
+               // console.log(edit);
+                update.onclick = function () {
+                    edit.style.display = 'inline';
+                    modle.setAttribute('contenteditable', true);
+                }
+                edit.onsubmit = function () {
+                    edit.style.display = 'inline'
+                }
             }
+             
+            
+            const edit = document.getElementById('to_edit');
+            const  model = document.getElementById('mod');
 
             // Get the <span> element that closes the modal
-            const span = document.getElementsByClassName("close")[0];
+            const span = document.getElementsByClassName('close')[0];
 
             // When the user clicks on <span> (x), close the modal
             span.onclick = function () {
-                modal.style.display = "none";
+                modal.style.display = 'none';
+                edit.style.display = 'none'
             }
         });
         //error handling
     }).catch(function (error) { 
-        console.log(error);
+        return error;
     });
+
+    
+document.getElementById('postData').addEventListener('submit', postData);
+
+
+function postData(event) {
+    return postBody;
+    const postUrl = url + newUrlData;
+    event.preventDefault();
+    return postUrl;
+    // The data we are going to send in our request
+    let data = document.getElementById('description').value;
+        
+    let good = {
+        'description' : `${data}`
+    };
+    // The parameters we are going pass to the fetch function
+    let fetchData = {
+        method: 'POST',
+        body: good,
+        headers: new Headers()
+    }
+    fetch(postUrl, fetchData)
+        .then(function (resp) {
+            console.log(resp.json())
+
+        })
+};
